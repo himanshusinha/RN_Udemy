84a886c01213cc05bc6d277e5e0a9e68b153a260
himanshusinha <himanshusinha011@gmail.com>
2026-01-07 23:51:36 +0530

    flexbox

--- App.tsx
+++ App.tsx
@@ -1,45 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import React from 'react';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goals" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals ...</Text>
+      </View>
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  appContainer: {
+    padding: 70,
   },
 });
-
 export default App;
